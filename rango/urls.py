1d93fcf52ab3edfff83918b04d99fe49b4920373
BeihongL <2721717L@student.gla.ac.uk>
2023-01-27 17:44:34 +0000

    第三次最终

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rango import views
+
 app_name = 'rango'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),  # 这里引号里面的空代表的就是http://...rango/这个链接直接就是主页的样子
+    path('about/', views.about, name='about'),  # 这里记得about后面价格斜杠，不然无法匹配对应的链接，因为rango/后的地址是交由这个urls文件处理的，后面出现的要打齐
 ]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -9,12 +9,18 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 模板目录
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# 静态目录
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +83,7 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -116,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# 静态文件目录
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
